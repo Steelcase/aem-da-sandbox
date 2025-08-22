67d36b3fbee3e77ad640815a88d4d1b3dd402bc6
Phil Kosiorowski <pkosioro@steelcase.com>
2025-08-22 16:07:58 -0400

    try to use secret in javascript file

--- blocks/helloworld/helloworld.js
+++ blocks/helloworld/helloworld.js
@@ -1,6 +1,8 @@
+const apiKey = process.env.ALGOLIA_API_KEY;
+
 export default function decorate(block) {
     const greeting = document.createElement('div');
-    greeting.textContent = 'Hello World:' + process.env.ALGOLIA_API_KEY;;
+    greeting.textContent = 'Hello World:' + apiKey;
     block.textContent = '';
     block.appendChild(greeting);
 }
