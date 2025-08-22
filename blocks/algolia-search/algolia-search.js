64cc9ce6d6be5ebac93a97212c95ddf3a9a439ef
MikeDeacon <mdeacon@steelcase.com>
2025-08-22 13:11:45 -0400

    Hello World Block Test

---
+++ blocks/algolia-search/algolia-search.js
@@ -0,0 +1,8 @@
+import "https://cdn.jsdelivr.net/npm/algoliasearch@5.35.0/dist/algoliasearch.umd.min.js";
+
+export default function decorate(block) {
+    const greeting = document.createElement('div');
+    greeting.textContent = 'Hello World';
+    block.textContent = '';
+    block.appendChild(greeting);
+}
