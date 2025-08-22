f3f8135c6468b6d81d1a21b6e8a7e686413ba69f
Phil Kosiorowski <pkosioro@steelcase.com>
2025-08-22 14:57:12 -0400

    added secret

--- blocks/helloworld/helloworld.js
+++ blocks/helloworld/helloworld.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
     const greeting = document.createElement('div');
-    greeting.textContent = 'Hello World';
+    greeting.textContent = 'Hello World:' + process.env.ALGOLIA_API_KEY;;
     block.textContent = '';
     block.appendChild(greeting);
 }
