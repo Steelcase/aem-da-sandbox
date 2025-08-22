f546e8a7a15c066663d2c0dce51d8c7a0831a817
MikeDeacon <mdeacon@steelcase.com>
2025-08-22 13:51:19 -0400

    Removing URl

--- blocks/algolia-search/algolia-search.js
+++ blocks/algolia-search/algolia-search.js
@@ -1,4 +1,4 @@
-import "https://cdn.jsdelivr.net/npm/algoliasearch@5.35.0/dist/algoliasearch.umd.min.js";
+//import "https://cdn.jsdelivr.net/npm/algoliasearch@5.35.0/dist/algoliasearch.umd.min.js";
 
 export default function decorate(block) {
     const greeting = document.createElement('div');
